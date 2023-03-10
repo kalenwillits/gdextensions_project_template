9a734999aa69441aacae333b82a4791c10bb66ca
Kalen <kalenwillits@gmail.com>
2023-03-10 09:32:08 -0500

    Added checking for gdclass

--- run/build_register_types.py
+++ run/build_register_types.py
@@ -31,14 +31,15 @@ def get_extension_headers(path_string="src/"):
         else:
             is_cpp_header = filename[-4:].lower() == ".hpp"
             is_c_header = filename[-2:].lower() == ".h"
+
             is_gdclass = False
-            with open(path / filename) as srcfile:
-                if "GDCLASS" in srcfile.readline():
-                    is_gdclass = True
+            if is_cpp_header or is_c_header:
+                with open(path / filename) as srcfile:
+                    if "GDCLASS" in srcfile.readline():
+                        is_gdclass = True
 
             if is_gdclass:
-                if is_cpp_header or is_c_header:
-                    results.append(GodotExtension(filename, path / filename))
+                results.append(GodotExtension(filename, path / filename))
 
     return results
 
